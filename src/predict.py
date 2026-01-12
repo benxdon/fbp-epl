34577c5250c21a8acfd092c0e3d2c01e6863a2f7
benxdon <brandonlin2006@gmail.com>
2026-01-12 12:53:35 +0700

    usable now, the user CLI has been updated

--- src/predict.py
+++ src/predict.py
@@ -1,6 +1,5 @@
 import joblib
 import pandas as pd
-from features import build_features
 from thefuzz import fuzz, process
 
 def process_input(inputs):
@@ -46,6 +45,7 @@ def process_input(inputs):
         direction="backward"
     ).rename(columns={"SOT5":"SOT5Home","Form5":"Form5Home"})
 
+
     # merge away
     df = pd.merge_asof(
         df.sort_values(by="MatchDate"),
@@ -55,31 +55,35 @@ def process_input(inputs):
         direction="backward"
     ).rename(columns={"SOT5":"SOT5Away","Form5":"Form5Away"})
 
+
+    #remove the values which have NA
+    df = df.dropna(subset=["SOT5Home","SOT5Away","Form5Home","Form5Away"])
+
     # features
     df["DiffElo"]   = df["HomeElo"]  - df["AwayElo"]
     df["DiffSOT5"]  = df["SOT5Home"] - df["SOT5Away"]
     df["DiffForm5"] = df["Form5Home"] - df["Form5Away"]
+    df["HomeAdv"] = 1
 
-    return df[["MatchDate","HomeTeam","AwayTeam","DiffElo","DiffSOT5","DiffForm5"]].reset_index(drop=True)
+    return df[["MatchDate","HomeTeam","AwayTeam","DiffElo","DiffSOT5","DiffForm5","HomeAdv"]].reset_index(drop=True)
 
 
-def predict_matches(raw_df):
+def predict_matches(inputs):
     
     model = joblib.load("model_lr.pkl")
-    features = build_features(raw_df)
 
-    X = features[["DiffElo","DiffForm5","HomeAdv","DiffSOT5"]]
+    X = inputs[["DiffElo","DiffForm5","HomeAdv","DiffSOT5"]]
     probs = model.predict_proba(X)[:,1]
 
-    features = features.assign(HomeWinProb = probs)
-    return features[["MatchDate","HomeTeam","AwayTeam","HomeWinProb"]]
+    inputs = inputs.assign(HomeWinProb = probs)
+    return inputs[["MatchDate","HomeTeam","AwayTeam","HomeWinProb"]]
 
 if __name__ == "__main__":
     
     matches = pd.read_csv("data/Matches.csv",low_memory=False)
     teams = pd.unique(pd.concat([matches["HomeTeam"],matches["AwayTeam"]]))
 
-    choice = int(input("Do you want to input manual or through CSV? 1. manual, 2. CSV: "))
+    choice = int(input("Do you want to input manual or through CSV? 1. CSV, 2. manual: "))
     
     if choice == 1:
         path = input("Please give the file name which is stored under the data/ folder: ")
@@ -87,21 +91,55 @@ if __name__ == "__main__":
         inputs = pd.read_csv(path, low_memory=False)
 
     elif choice == 2: 
-        homeTeam, homeElo = input("Please input the home team and their ELO (team_name elo_rating): ").split()
-        matches = process.extract(homeTeam, teams, limit=3)
 
-        while matches[0][1] != 100:
-            print("We found the corresponding matches")
-            print(matches)
-            homeTeam = input("Please input the home team again: ")
-        
-        awayTeam, awayElo = input("Please input the away team and their ELO (team_name elo_rating): ").split()
-        matches = process.extract(awayTeam, teams, limit=3)
+        while True:
+            
+            homeTeam, homeElo = input("Please input the home team and their ELO (team_name elo_rating): ").split()
+            matches = process.extract(homeTeam, teams, limit=3)
+            
+            best_team, best_score = matches[0]
 
-        while matches[0][1] != 100:
-            print("We found the corresponding matches")
-            print(matches)
-            awayTeam = input("Please input the away team again: ")
+            print(f"Best match: {best_team} ({best_score}%)")
+            confirm = input("Use this team? (y/n): ").lower()
+
+            if confirm == "y":
+                homeTeam = best_team
+                break
+
+            print("Other possible matches: ")
+            for i, (team, score) in enumerate(matches):
+                print(f"{i+1}. {team} ({score})")
+
+            choice = int(input("Choose 1-3: "))
+            
+            if choice >= 1 and choice <= 3:
+                homeTeam = matches[choice-1][0]
+                break
+            
+        while True:
+            
+            awayTeam, awayElo = input("Please input the away team and their ELO (team_name elo_rating): ").split()
+            matches = process.extract(awayTeam, teams, limit=3)
+            
+            best_team, best_score = matches[0]
+
+            print(f"Best match: {best_team} ({best_score}%)")
+            confirm = input("Use this team? (y/n): ").lower()
+
+            if confirm == "y":
+                awayTeam = best_team
+                break
+
+            print("Other possible matches: ")
+            for i, (team, score) in enumerate(matches):
+                print(f"{i+1}. {team} ({score})")
+
+            choice = int(input("Choose 1-3: "))
+            
+            if choice >= 1 and choice <= 3:
+                awayTeam = matches[choice-1][0]
+                break
+
 
         date = input("What is the Match Date (YYYY-MM-DD): ")
 
